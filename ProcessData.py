6ef253d3b1e44da701d9473b03b36c3221165582
k754a <kallenms27@gmail.com>
2026-06-27 11:53:26 -0600

    First Commit! Some of the tokenizer made! and first changes (aswell as dataset)

---
+++ ProcessData.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+#because its a .csv file, first we are gonna convert our dataset to a txt file, removing some of the headers and stuff
+
+#this funct will handle the data, for our main Poem Generator
+def HandleData():
+    #first lets open our file
+
+    p = pd.read_csv("poetry.csv", usecols=['content'])
+
+    print(p)
+
+    ##each row is a poem, so lets conver it into a txt file, so we can work with it easier
+
+    #for each poem in the content column, we add it to the file
+    with open("poems.txt", "w", encoding="utf-8") as f:
+        for poem in p['content']:
+            #add the poem, and then make a new line
+            f.write(poem + "\n")
+
+
+    #a list of chars that we dont want
+    remove_chars = r"""/\.,:;'+=-|"'!@#$%^&*())1234567890"""
+    #this will also handle the removing unwanted punctuation and chars:
+    #we use with open, as it handles closing the file for us
+    finallines = ""
+    with open("poems.txt", "r", encoding="utf-8") as f:
+        #ok, for each line, in the file, lets remove the "/\.,:;'+=-| and whatever other chars there are
+        for line in f:
+            #the way it works, is it takes in a line, and attempts to translate an chars to whatever i set. we use this instead of replace
+            #because it lets me do multiple making a transition table.
+            cleanline = line.translate(str.maketrans("", "", remove_chars))
+            finallines += cleanline;
+
+
+
+        
+    print(finallines)
+
+    #the W mode, removes all text on open
+    with open("poems.txt", "w") as file:
+        file.write(finallines)
+
+
+    print("Done!")
+
+
+
+
+
+
+
+
+
---
+++ Tokenizer.py
@@ -0,0 +1,46 @@
+#ok this is our tokenizer.
+
+#We are building a RNN from scratch, a type of ai network. im using a tutorial as ive never touched this before!
+numTokens = [] #global
+
+
+def Token():
+    # we have our data, in a txt file, we need to split it and rank it from the most occurrences to the list.
+    full = ""
+    with open("poems.txt", "r", encoding="utf-8") as f:
+        full = f.read()
+
+    #now we have the full text, lets make a list
+
+    words = full.split() #split into words
+
+    word_counts = {} # Using a dictionary
+
+
+    #for each word in words
+    for word in words:
+
+        if word in word_counts: #checks if we have this saved in our dict
+            word_counts[word] +=1 #add one to the dict
+        else: #add it to our dict
+            word_counts[word] =1 #make a new dict, and set it to 1
+        
+
+    wordID = {}
+    current_id = 1
+    #now we want to assign an ID to each word, as we have the counts, but not the id, and the rank from biggest to smallest
+    for word in word_counts.keys():
+        wordID[word] = current_id
+
+        current_id +=1
+
+
+    #ok, now we want to translate the poem
+
+    for word in words:
+        #we assign each word to a token that our id thing did
+        numTokens.append(wordID[word])
+
+
+
+    print("Done!")
